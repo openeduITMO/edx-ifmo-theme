61b88dc2ce793a157b0d52fffced3032eac93089
Dmitry Ivanyushin <defance@gmail.com>
2015-11-20 15:41:56 +0300

    Поправлено позиционирование курсов при фильтре

--- static/js/ifmo-catalog.js
+++ static/js/ifmo-catalog.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+
+    var cache = {};
+
+    var $time_filters = $(".time-filters li");
+    var $category_filters = $(".category-filters");
+    var $courses = $(".course");
+    var $courses_container = $('.courses-listing.ifmo-listing');
+    var filters = [];
+
     //Удаляет фильтр установленный на любом из елементов group из общего списка текущих фильтров (filters)
     function delete_group_from_filter(group){
         var new_filter = [];
@@ -11,16 +20,41 @@ $(document).ready(function(){
     }
 
     //Оставляет видными только те элементы, у которых в аттрибуте attr присутствует значение attr_value
-    function show_if_contains_attr($elems, attr, attr_value){
-        $elems.filter(":not(["+attr+"~='"+attr_value+"'])").parent().hide();
+    function cache_courses($elems, attr, attr_value){
+        $elems.filter(":not(["+attr+"~='"+attr_value+"'])").each(function(){
+            var $parent = $(this).parent();
+            cache[$parent.index()] = $parent;
+        });
     }
 
     //Применяет ко всем курсам текущие установленные фильтры (filters)
     function apply_filters(){
-        $courses.parent().show();
+        $.each(cache, function(index, $course){
+            index = parseInt(index);
+            if(index == 0) {
+                $courses_container.prepend($course);
+            } else {
+                var to_add = index + 1;
+                while (cache[to_add]) to_add += 1;
+                if (to_add == $courses.length) {
+                    $courses_container.append($course);
+                } else {
+                    // TODO: Красивый селектор
+                    // Возможно, есть более изящный способ получить позицию
+                    // элемента изначальную позицию элемента, например, с самого
+                    // начала записывать в него самого.
+                    var course_selector = 'article[id="' + $($courses[to_add]).attr('id') + '"]';
+                    $course.insertBefore($courses_container.find(course_selector).parent());
+                }
+            }
+            delete cache[index];
+        });
         filters.forEach(function(filter){
-            show_if_contains_attr($courses, filter["attr_name"], filter["value"]);
-        })
+            cache_courses($courses, filter["attr_name"], filter["value"]);
+        });
+        $.each(cache, function(i, val){
+            val.remove();
+        });
     }
 
     //Устанавливает событие нажатия на один из фильтров и добавляет фильтр в общий список filters
@@ -43,11 +77,6 @@ $(document).ready(function(){
             apply_filters();
         })
     }
-
-    var $time_filters = $(".time-filters li");
-    var $category_filters = $(".category-filters");
-    var $courses = $(".course");
-    var filters = [];
 
     set_filter_event($time_filters, "data-ifmo-course-state");
     $category_filters.each(function(){
